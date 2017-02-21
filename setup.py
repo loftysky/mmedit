5501d25e06e0ca451418fb53c8a9535cb9b93427
Mike Boers <mikeb@markmedia.co>
2017-02-21 11:34:40 -0500

    Stub out the basic package (with zero content).

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mmedit',
+    version='0.1.dev0',
+    description='Lofty Sky\'s general editorial tools.',
+    url='http://github.com/loftysky/mmedit',
+    
+    packages=find_packages(exclude=['build*', 'tests*']),
+
+    author='Lofty Sky Development Team',
+    author_email='opensource@loftysky.com',
+
+    license='BSD-3',
+    
+    entry_points={
+        'console_scripts': '''
+
+            # TODO: Scripts.
+
+        ''',
+    },
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    
+)
