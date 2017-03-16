160027692040398c31fa53ea26187a0245f44fa8
Elaine Wong <elainew@markmedia.co>
2017-03-16 17:29:14 -0400

    adding function to_link()

--- mmedit/footage/ingest.py
+++ mmedit/footage/ingest.py
@@ -5,12 +5,29 @@ import re
 import os
 import hashlib
 import uuid
+import errno
 
 from sgfs import SGFS
 from sgpublish import Publisher
 
 
 VALID_EXT = set(('.mp4', '.avi', '.mts', '.mov'))
+
+def create_element(set_, data, sg):
+    '''Creates a single Element on Shotgun.
+
+    sg.
+    TODO (later):
+    - Create a thumbnail/filmstrip (ffmpeg is okay for now, PyAV later).
+    - Create a proxy on the farm (ffmpeg is okay).
+    - Publish with sgpublish.
+    - Create a Version (likely with sgpublish Version tools).
+    '''
+
+    data = data.copy()
+    data['sg_element_set'] = set_
+    data['project'] = set_["project"]
+    sg.create('Element', data)
 
 
 def create_element_set(set_data, element_data, sgfs):
@@ -35,32 +52,15 @@ def create_element_set(set_data, element_data, sgfs):
 
     sg.update('$ElementSet', element_set['id'], set_data)
 
-
-def create_element(set_, data, sg):
-    '''Creates a single Element on Shotgun.
-
-    sg.
-    TODO (later):
-    - Create a thumbnail/filmstrip (ffmpeg is okay for now, PyAV later).
-    - Create a proxy on the farm (ffmpeg is okay).
-    - Publish with sgpublish.
-    - Create a Version (likely with sgpublish Version tools).
-    '''
-    '''
-    sgfs = SGFS()
-    sg = sgfs.session
-    entity = sg.create('entity', data)
-    '''
-    data = data.copy()
-    data['sg_element_set'] = set_
-    data['project'] = set_["project"]
-    sg.create('Element', data)
+    return element_set
 
     
 def is_footage(path):
     name, ext = os.path.splitext(os.path.basename(path))           
     if ext.lower() in VALID_EXT and not name.startswith('.'):
         return True
+
+
 
 def main():
     '''
@@ -78,6 +78,7 @@ def main():
 
     '''
     sgfs = SGFS()
+    sg = sgfs.session
     approvedFootageList = []
     unapprovedFootageList = []
     data = {}
@@ -91,6 +92,7 @@ def main():
         help='Dont do anything.')
     parser.add_argument('fdirectory')
     parser.add_argument('-P', '--project', help="Path to project/url/id")
+    parser.add_argument ('destination')
     args = parser.parse_args()
 
 
@@ -112,7 +114,7 @@ def main():
         for name in files:
             path = os.path.join(root, name)
             rel_path = os.path.relpath(path, args.fdirectory)
-            filename, _ = os.path.splitext(name)            
+            filename, ext = os.path.splitext(name)            
             if is_footage(path):
                 print (rel_path)
                 approvedFootageList.append({
@@ -127,12 +129,66 @@ def main():
     if not args.yes:
         answer = raw_input("Do you want to import this footage? [Y or n?] ")
         if not answer.strip().lower() == "y":
-            exit()
-    create_element_set({
-        'code': args.name or os.path.basename(args.fdirectory),
-        'sg_path': args.fdirectory,
-        'project': project,
-    }, approvedFootageList, sgfs)
+            exit(3)
+    if not args.dry_run:
+        element_code = create_element_set({
+            'code': args.name or os.path.basename(args.fdirectory),
+            'sg_path': args.fdirectory,
+            'project': project,
+        }, approvedFootageList, sgfs)
+        element_code_id = element_code['id']
+
+    element_code_id = 4
+    element_set = sg.find('Element', [('sg_element_set', 'is', {'type': 'CustomEntity27', 'id': element_code_id})], ['sg_path', 'sg_uuid', 'sg_relative_path', 'sg_element_set.CustomEntity27.code'])
+
+    to_link(element_set, args.destination, sg, symlinks=True)
+
+
+
+def to_link(element_set, dest_path, sg, symlinks):
+    '''
+    takes the element set path, sets the new path to hard or symlinks dependent on link option
+    '''
+    try:
+        os.makedirs(dest_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    '''
+    try:
+        change_path = os.path.join(str(dest_path), str(element_set[0]['sg_element_set.CustomEntity27.code']))
+        os.makedirs(change_path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    '''
+    for item in element_set:
+        sg_path = item['sg_path']
+        dirs, filename = os.path.split(item['sg_relative_path'])
+        filename, ext = os.path.splitext(filename)
+        new_dirs = os.path.join(str(dest_path), str(element_set[0]['sg_element_set.CustomEntity27.code']), dirs)
+        try: 
+            os.makedirs(new_dirs)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        new_filename = filename + "_" + item['sg_uuid'][:8 ] + ext
+        dest_filename = os.path.join(new_dirs, new_filename)
+        if symlinks == False: 
+            try:
+                os.link(sg_path, dest_filename)
+            except OSError as e:
+                if errno != errno.EEXIST:
+                    raise
+        try: 
+            os.symlink(sg_path, dest_filename)
+        except OSError as e:
+            if errno != errno.EEXIST:
+                raise
+        print('Creating symlink for file to ....', dest_filename)
+     
+
+
 
 
 
